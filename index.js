9cb00d72f3c0fa76748dd91e9175778ce13e5225
Mohd Abdul Muqtadeer <91531470+saadmuqtadeer@users.noreply.github.com>
2023-11-03 01:04:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var n = document.querySelectorAll(".drum").length;
+for (var i = 0; i < n; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var button = this.innerHTML;
+        makesounds(button);
+        anime(button);
+    });
+}
+document.addEventListener("keypress", function (event){
+    makesounds(event.key);
+    anime(event.key);
+});
+
+function makesounds(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        default:console.log(button);
+            break;
+    }
+}
+
+function anime(key){
+    var activebutton = document.querySelector("."+key);
+    activebutton.classList.add("pressed");
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    }, 100);
+}
